eed770fe28c97c0c0b7ed746435a53159a3da1a9
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2024-02-28 12:54:14 +0200

    Use the rust fuzzy matcher in Blink.cmp
    i.e. the option "prefer_rust_with_warning"

--- init.lua
+++ init.lua
@@ -894,7 +894,7 @@ do
     -- the rust implementation via `'prefer_rust_with_warning'`
     --
     -- See `:help blink-cmp-config-fuzzy` for more information
-    fuzzy = { implementation = 'lua' },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
 
     -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
