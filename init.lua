92e6284b8a2caedadd09c8ab9dd699c1d9678152
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2024-02-28 13:07:00 +0200

    Disable current line highlight

--- init.lua
+++ init.lua
@@ -148,7 +148,8 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+-- I don't like this...
+-- vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
