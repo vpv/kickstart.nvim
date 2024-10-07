af6a42f50e40021826e6230de1256b189e05dfce
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2024-10-07 10:48:40 +0300

    Enable nerd font

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 -- this is needed for nvim-tree
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
