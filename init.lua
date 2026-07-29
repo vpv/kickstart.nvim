0ba0730665d2ab15e586ae4baa856fe335ec0628
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2026-07-29 13:41:37 +0300

    Use the nightfox colorscheme

--- init.lua
+++ init.lua
@@ -395,10 +395,13 @@ do
     },
   }
 
+  vim.pack.add { gh 'EdenEast/nightfox.nvim' }
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  -- vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'nightfox'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
