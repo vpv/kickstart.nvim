924f35c2290228f716865b1279643b2cf9cd7a3a
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2026-08-05 13:39:36 +0300

    Enable pyright

--- init.lua
+++ init.lua
@@ -702,7 +702,7 @@ do
   local servers = {
     -- clangd = {},
     -- gopls = {},
-    -- pyright = {},
+    pyright = {},
     -- rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
