b644b8209f2a7c0df8d5f383358571f65a46230f
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2026-08-07 14:03:58 +0300

    Enable friendly-snippets

--- init.lua
+++ init.lua
@@ -841,8 +841,8 @@ do
   --    See the README about individual language/framework/plugin snippets:
   --    https://github.com/rafamadriz/friendly-snippets
   --
-  -- vim.pack.add { gh 'rafamadriz/friendly-snippets' }
-  -- require('luasnip.loaders.from_vscode').lazy_load()
+  vim.pack.add { gh 'rafamadriz/friendly-snippets' }
+  require('luasnip.loaders.from_vscode').lazy_load()
 
   -- [[ Autocomplete Engine ]]
   vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
